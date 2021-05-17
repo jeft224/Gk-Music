5bc5eef4b7ccb4e10ed89f9abec2c6a2da1ac461
abert <zhiwei.luo@zuolin.com>
2021-05-17 18:22:53 +0800

    优化排行榜模块页面的展示

--- app/src/views/recommend/components/rank/index.tsx
+++ app/src/views/recommend/components/rank/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+import './index.less'
+
 const prefixCls = 'rank'
 
 interface RankProps {}
@@ -37,12 +39,47 @@ export default class Rank extends React.Component<RankProps, RankState> {
 
   render(): JSX.Element {
     const { ranks } = this.state
-    const { list } = ranks
+    const { list, artistToplist } = ranks
+    const topRankList = list?.filter((item) => item.tracks.length)
+    const topSubRankList = list?.filter((item) => !item.tracks.length)
     return (
       <div className={prefixCls}>
-        <h3>rank</h3>
+        <h3>官方榜</h3>
+        <ul className={`${prefixCls}-top`} style={{ marginBottom: '20px' }}>
+          {topRankList?.map(({ coverImgUrl, id, tracks }: queryRankUsingGET.RankItem) => {
+            return (
+              <li key={id} className="rank-item">
+                <img className="rank-image" src={coverImgUrl} alt="" />
+                <div className="rank-right">
+                  {tracks?.map(({ first, second }) => {
+                    return (
+                      <p className="rank-song" key={first}>
+                        {first}/{second}
+                      </p>
+                    )
+                  })}
+                  <p className="music-name">全部</p>
+                </div>
+              </li>
+            )
+          })}
+          <li className="rank-item">
+            <img className="rank-image" src={artistToplist?.coverUrl} alt="" />
+            <div className="rank-right">
+              {artistToplist?.artists?.map(({ first }) => {
+                return (
+                  <p className="rank-song" key={first}>
+                    {first}
+                  </p>
+                )
+              })}
+              <p className="music-name">全部</p>
+            </div>
+          </li>
+        </ul>
+        <h3>全球榜</h3>
         <ul className="music-list">
-          {list?.map(({ name, coverImgUrl, id }: queryRankUsingGET.RankItem) => {
+          {topSubRankList?.map(({ name, coverImgUrl, id }: queryRankUsingGET.RankItem) => {
             return (
               <li key={id} className=" music-item flex column  center">
                 <img className="music-image" src={coverImgUrl} alt="" />
